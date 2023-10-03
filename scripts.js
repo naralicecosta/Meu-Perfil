7a7c2499a6f522cb2ba1b9ecb61b5c77d3d8c3c8
naralicecosta <naralicecosta04@gmail.com>
2023-10-02 22:44:58 -0300

    concluindo

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@ function toggleMode() {
   //html.classList.add("light")
   //
 
-  //pegar a tag img
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/Avatar.png")
